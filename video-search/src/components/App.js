2b242eaae121149aa9ddc9fe1f11cce13861e78e
VaderSteve76 <alverson.s@yahoo.com>
2021-09-03 14:14:53 -0400

    VideoList component

--- video-search/src/components/App.js
+++ video-search/src/components/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from '../components/VideoList';
 
 export default class App extends Component {
 
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = {
+    videos: []
+  }
+
+  onTermSubmit = async (term) => {
+    const res = await youtube.get('/search', {
       params: {
         q: term
       }
-    })
+    });
+    this.setState({ videos: res.data.items })
   }
 
   render() {
     return (
       <div>
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
