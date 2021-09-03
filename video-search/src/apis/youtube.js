f4ba4c43f736214d2764873eac38a7b482e3325c
VaderSteve76 <alverson.s@yahoo.com>
2021-09-03 14:00:07 -0400

    Getting response from api

--- video-search/src/apis/youtube.js
+++ video-search/src/apis/youtube.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCEep__WoeUe8uzvDHvXQJWcnlET1Pn5hY";
+const KEY = "AIzaSyAI3EAjfg850e4EohnF7Pc-MCVos1re8N4";
 
 export default axios.create({
-  baseURL: "https;//www.googleapis.com/youtube/v3",
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
+    type: "video",
     maxResults: 5,
     key: KEY
   }
